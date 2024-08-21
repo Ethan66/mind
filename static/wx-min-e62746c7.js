6e4158b3e05840b7053d3948f1368544bc75b885
zc <2064281269@qq.com>
2024-08-21 15:14:43 +0800

    deploy

--- static/wx-min-e62746c7.js
+++ static/wx-min-e62746c7.js
@@ -2,7 +2,7 @@ const e="logicalStructure",t={data:{text:"小程序首页",expand:!0,isActive:!1
 （首页不用给弹窗，直接进）`,uid:"a0ac17e9-62bd-4b0f-8713-33638e2e8788",expand:!0,richText:!1,isActive:!1,borderWidth:1,borderColor:"#549688",fillColor:"#fff"},children:[{data:{text:`征信协议（/unicom/auth/loading）：
 created：一推页面调接口判断要不要展示征信协议（接口：/app/show/agree）：
    1. data.needShow == 1：是新用户 || 老客征信开关打开，要展示协议
-   2. data.needShow == 0：是老用户(借款笔数>=1) || 老客征信开关关闭，不要展示协议
+   2. data.needShow == 0：是老用户(借款笔数>=1) && 老客征信开关关闭，不要展示协议
 `,expand:!0,richText:!1,isActive:!1,borderWidth:1,borderColor:"#549688",fillColor:"#fff",uid:"1f8cb796-6c83-4943-bbb6-c8dcd0faa8b7"},children:[]},{data:{text:`一推：
 1. 安卓调获取最新的环境参数，再走一推
 2. 其他直接走一推
@@ -19,4 +19,4 @@ created：一推页面调接口判断要不要展示征信协议（接口：/app
 
         
         
-        `,uid:"69e57bbb-caae-41b9-9c27-55a5b01245dd",expand:!0,richText:!1,isActive:!1,borderWidth:1,borderColor:"#549688",fillColor:"#fff"},children:[]}]},{data:{text:"当前资方无在贷，去正常流程",expand:!0,richText:!1,isActive:!1,uid:"c5ec8e1f-8465-4a58-a3be-03b701597ed0",borderWidth:1,borderColor:"#549688",fillColor:"#fff"},children:[]}]}]}]},a={template:"default",config:{associativeLineTextFontSize:13,root:{fontSize:14},node:{fontSize:13},generalization:{fontSize:13},second:{fontSize:13}}},d={transform:{scaleX:1,scaleY:1,shear:0,rotate:0,translateX:96,translateY:0,originX:0,originY:0,a:1,b:0,c:0,d:1,e:96,f:0},state:{scale:1,x:96,y:0,sx:96,sy:0}},r={layout:e,root:t,theme:a,view:d};export{r as default,e as layout,t as root,a as theme,d as view};
+        `,uid:"69e57bbb-caae-41b9-9c27-55a5b01245dd",expand:!0,richText:!1,isActive:!1,borderWidth:1,borderColor:"#549688",fillColor:"#fff"},children:[]}]},{data:{text:"当前资方无在贷，去正常流程",expand:!0,richText:!1,isActive:!1,uid:"c5ec8e1f-8465-4a58-a3be-03b701597ed0",borderWidth:1,borderColor:"#549688",fillColor:"#fff"},children:[]}]}]}]},a={template:"default",config:{associativeLineTextFontSize:13,root:{fontSize:14},node:{fontSize:13},generalization:{fontSize:13},second:{fontSize:13}}},d={transform:{scaleX:1.2,scaleY:1.2,shear:0,rotate:0,translateX:-150.9714285714287,translateY:-.6857142857143401,originX:0,originY:0,a:1.2,b:0,c:0,d:1.2,e:-150.9714285714287,f:-.6857142857143401},state:{scale:1.2,x:-150.9714285714287,y:-.6857142857143401,sx:-150.9714285714287,sy:-.6857142857143401}},r={layout:e,root:t,theme:a,view:d};export{r as default,e as layout,t as root,a as theme,d as view};
